ed02aa3935e1784c995750d2807db21a9537d020
javainajiffy <142672715+javainajiffy@users.noreply.github.com>
2023-09-16 18:08:40 -0400

    Add files via upload

---
+++ programs/2/solution/Program2.java
@@ -0,0 +1,10 @@
+package com.javainajiffy;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Brian");
+		System.out.println("My birthday is September 5");
+	}
+
+}

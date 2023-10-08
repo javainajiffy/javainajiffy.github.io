c827ba3a30368b9fb68be7bbaf03ce56c70b70ab
javainajiffy <142672715+javainajiffy@users.noreply.github.com>
2023-10-08 19:50:46 -0400

    Add files via upload

---
+++ programs/3/solution/Program3.java
@@ -0,0 +1,23 @@
+package com.javainajiffy;
+
+public class Program3 {
+
+	public static void main(String[] args) {
+		System.out.println(" _____________________________________________________________________");
+		System.out.println("|                                                                     |");
+		System.out.println("|     1. Clearly define what the program is to do.                    |");
+		System.out.println("|     2. Visualize the program running on the computer.               |");
+		System.out.println("|     3. Use design tools such as a hierarchy chart, flowcharts,      |");
+		System.out.println("|        or pseudocode to create a model of the program.              |");
+		System.out.println("|     4. Check the model for logical errors.                          |");
+		System.out.println("|     5. Type the code, save it, and compile it.                      |");
+		System.out.println("|     6. Correct any errors found during compilation.                 |");
+		System.out.println("|        Repeat steps 5 and 6 as many times as necessary.             |");
+		System.out.println("|     7. Run the program with test data for input.                    |");
+		System.out.println("|     8. Correct any errors found while running the program.          |");
+		System.out.println("|        Repeat steps 5 through 8 as many times as necessary.         |");
+		System.out.println("|     9. Validate the results of the program.                         |");
+		System.out.println("|_____________________________________________________________________|");
+	}
+
+}

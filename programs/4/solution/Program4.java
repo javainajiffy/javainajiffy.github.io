4c25f755c8ec1164c5e545735b3ea217b01b319a
javainajiffy <142672715+javainajiffy@users.noreply.github.com>
2023-10-08 20:49:40 -0400

    Add files via upload

---
+++ programs/4/solution/Program4.java
@@ -0,0 +1,11 @@
+package com.javainajiffy;
+
+public class Program4 {
+
+	public static void main(String[] args) {
+		System.out.println("The teacher entered the classroom and said, \"Good Morning\".");
+		System.out.println("She then wrote the following message on the board, \"Today's Date: 9\\16\\2023\".");
+		
+	}
+
+}

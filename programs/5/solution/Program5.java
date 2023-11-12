d66b2f65d0613f2e4a2380f4449c0d6af3ed6afd
javainajiffy <142672715+javainajiffy@users.noreply.github.com>
2023-11-12 13:07:08 -0500

    Add files via upload

---
+++ programs/5/solution/Program5.java
@@ -0,0 +1,12 @@
+package com.javainajiffy;
+
+public class Program5 {
+
+	public static void main(String[] args) {
+		String var = "This is a String";
+		String thisToo = "this is too";
+
+		System.out.println(var + " and " + thisToo + "!");
+	}
+
+}

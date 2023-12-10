62edf918f65138c4fe140b67854920afccae742b
javainajiffy <142672715+javainajiffy@users.noreply.github.com>
2023-12-10 10:55:21 -0500

    Add files via upload

---
+++ programs/7/solution/Program7.java
@@ -0,0 +1,17 @@
+package com.javainajiffy;
+
+public class Program7 {
+
+	public static void main(String[] args) {
+		int num1 = 28;
+		int num2 = 32;
+		int num3 = 37;
+		int num4 = 24;
+		int num5 = 33;
+		double average = (num1 + num2 + num3 + num4 + num5) / 5.0;
+		System.out.println("The average of " + num1 + ", " + num2 + ", " 
+				    + num3 + ", " + num4 + ", and " + num5 + " is: " + average);
+
+	}
+
+}

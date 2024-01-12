1e6dd6c17e177c8644210b12b8434c03ab26dfa8
javainajiffy <142672715+javainajiffy@users.noreply.github.com>
2024-01-11 20:42:30 -0500

    Add files via upload

---
+++ programs/9/solution/Program9.java
@@ -0,0 +1,19 @@
+package com.javainajiffy;
+
+public class Program9 {
+
+	public static void main(String[] args) {
+		String name = "Bob Brown";
+		int age = 53;
+		double weight = 226.4;
+		char middleInitial = 'W';
+		boolean citizen = true;  
+		
+		System.out.println("The value stored in the string variable is: " + name );
+		System.out.println("The value stored in the int variable is: " + age );
+		System.out.println("The value stored in the double variable is: " + weight );
+		System.out.println("The value stored in the char variable is: " + middleInitial );
+		System.out.println("The value stored in the boolean variable is: " + citizen );
+	}
+
+}

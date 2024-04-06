8ebb8c6796507d69824b2aa36f61ee777509b282
javainajiffy <142672715+javainajiffy@users.noreply.github.com>
2024-04-06 11:46:39 -0400

    Add files via upload

---
+++ programs/12/solution/Program12.java
@@ -0,0 +1,18 @@
+package com.javainajiffy;
+
+import java.util.Scanner;
+
+public class Program12 {
+
+	public static void main(String[] args) {
+		System.out.println("Enter weight in pounds: ");
+
+		Scanner s = new Scanner(System.in);
+		double pounds = s.nextDouble();
+		
+		double kilograms = pounds / 2.21;
+	
+		System.out.println("Weight in kilograms: " + kilograms);
+	}
+
+}
